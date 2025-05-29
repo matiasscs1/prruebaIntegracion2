70d512551b16c4f2c046919dc6975ca617dfd43b
matiasscs1 <matias.cedeno@udla.edu.ec>
2025-05-28 21:12:29 -0500

    puerto

--- SolicitudService/src/index.js
+++ SolicitudService/src/index.js
@@ -3,7 +3,7 @@ import {connectDB} from './db.js';
 
 connectDB();
 
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 6001;
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
